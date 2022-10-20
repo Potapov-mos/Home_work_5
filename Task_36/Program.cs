489c22c8fe5c502b76c13efc08c8f6b40939dd50
Dmitriy <potapidze.mos@mail.ru>
2022-10-20 20:37:11 +0300

    Добавил решение задачи №36

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -6,3 +6,44 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+
+
+int[] OddElem(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+void SumOddElem(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum += array[i];
+        }
+    }
+    Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
+}
+
+int[] arr = OddElem(4, -100, 100);
+PrintArray(arr);
+SumOddElem(arr);
